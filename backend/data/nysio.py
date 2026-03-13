8ddb2f95889d4c1a839ba17eaefa97fb9d8620f3
Azeel Sajjad <aasajjad05@gmail.com>
2026-03-13 12:35:58 -0400

    automated data pipelines

--- backend/data/nysio.py
+++ backend/data/nysio.py
@@ -1,30 +1,51 @@
 import pandas as pd
 import requests
-from models.prices import Price
-from models.load import Load
 from db import engine
 import io
+from apscheduler.schedulers.background import BackgroundScheduler
+import time
+from datetime import datetime
 
-def fetch_price(date):
-    date = date.strip('-')
+def fetch_price():
+    date = datetime.now().strftime('%Y%m%d')
     url = f"https://mis.nyiso.com/public/csv/rtlbmp/{date}rtlbmp_zone.csv"
     response = requests.get(url)
     if response.status_code == 200:
         print("Reached page")
+        with engine.begin() as connection:
+            df = pd.read_csv(io.StringIO(response.text))
+            df.to_sql('prices', con=connection, if_exists='append', index=False)
     else:
-        print("Failed to reach page. Status code: {response.status_code}")
-    with engine.begin() as connection:
-        df = pd.read_csv(io.StringIO(response.text))
-        df.to_sql('prices', con=connection, if_exists='append', index=False)
+        print(f"Failed to reach page. Status code: {response.status_code}")
         
-def fetch_load(date):
-    date = date.strip('-')
+def fetch_load():
+    date = datetime.now().strftime('%Y%m%d')
     url = f"https://mis.nyiso.com/public/csv/pal/{date}pal.csv"
     response = requests.get(url)
     if response.status_code == 200:
         print("Reached page")
+        with engine.begin() as connection:
+            df = pd.read_csv(io.StringIO(response.text))
+            df.to_sql('load', con=connection, if_exists='append', index=False)
     else:
-        print("Failed to reach page. Status code: {response.status_code}")
-    with engine.begin() as connection:
-        df = pd.read_csv(io.StringIO(response.text))
-        df.to_sql('load', con=connection, if_exists='append', index=False)
+        print(f"Failed to reach page. Status code: {response.status_code}")
+        
+if __name__ == '__main__':
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(
+        func=fetch_price,
+        trigger='interval',
+        hours=1
+    )
+    scheduler.add_job(
+        func=fetch_load,
+        trigger='interval',
+        hours=1
+    )
+    scheduler.start()
+    try:
+        while True:
+            time.sleep(2)
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown()
+    
